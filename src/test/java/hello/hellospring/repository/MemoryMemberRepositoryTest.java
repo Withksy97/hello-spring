599bd4ba0dd0b8822e2f363c13ee38a092e23cf4
withksy97 <cmoojin21@naver.com>
2023-01-12 22:33:07 +0900

    Make TestCode

--- src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
+++ src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -1,7 +1,10 @@
 package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -9,8 +12,13 @@ import static org.junit.jupiter.api.Assertions.*;
 public class MemoryMemberRepositoryTest {
     MemoryMemberRepository repository = new MemoryMemberRepository();
 
+    @AfterEach
+    public void afterEach() {
+        repository.clearStore();
+    }
+
     @Test
-    public void save(){
+    public void save() {
         Member member = new Member();
         member.setName("spring");
 
@@ -23,7 +31,7 @@ public class MemoryMemberRepositoryTest {
     }
 
     @Test
-    public void findByName(){
+    public void findByName() {
         Member member1 = new Member();
         member1.setName("spring1");
         repository.save(member1);
@@ -33,5 +41,10 @@ public class MemoryMemberRepositoryTest {
         Member member2 = new Member();
         member2.setName("spring2");
         repository.save(member2);
+
+        Member result = repository.findByName("spring1").get();
+
+        assertThat(result).isEqualTo(member1);
     }
+
 }
